a5c20eef2e2c5bb549182becb96748f9fb2cc060
Renan Pinho <pinhorenan@icloud.com>
2023-02-25 15:15:34 -0300

    refactor

--- home/views.py
+++ home/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from utils.recipes.factory import make_recipe
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'home/pages/home.html')
+    return render(request, 'home/pages/home.html', context={
+        'recipes': [make_recipe() for _ in range(10)],
+    })
 
 def recipe(request, recipe_id):
-    return render(request, 'home/pages/recipe-view.html')
+    return render(request, 'home/pages/recipe-view.html', context={
+        'recipe': make_recipe(),
+    })
 
 
