b57c322bb6f91582801f177898089c666f086f14
Renan Pinho <pinhorenan@icloud.com>
2023-02-26 01:32:18 -0300

    entering the night... making progress...

--- config/settings.py
+++ config/settings.py
@@ -124,6 +124,11 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 
+STATIC_ROOT = BASE_DIR /'static_root'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
--- home/models.py
+++ home/models.py
@@ -7,6 +7,9 @@ from django.contrib.auth.models import User
 class Category(models.Model):
     name = models.CharField(max_length=65)
 
+    def __str__(self):
+        return self.name
+
 class Recipe(models.Model):
     title = models.CharField(max_length=65)
     description = models.CharField(max_length=165)
@@ -23,3 +26,6 @@ class Recipe(models.Model):
     cover = models.ImageField(upload_to='home/covers/%Y/%m/%d', null=True)
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.title
