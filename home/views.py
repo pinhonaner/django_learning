7a467570455ee5bf8b386fc2dd2af1b7df090e63
Renan Pinho <pinhorenan@icloud.com>
2023-02-27 07:50:29 -0300

    minor update

--- home/views.py
+++ home/views.py
@@ -5,9 +5,7 @@ from .models import Recipe
 # Create your views here.
 
 def home(request):
-    recipes = Recipe.objects.filter(
-        is_published=True
-    ).order_by('-id')
+    recipes = get_list_or_404(Recipe.objects.filter(is_published=True).order_by('-id'))
     return render(request, 'home/pages/home.html', context={
         'recipes': recipes,
         
@@ -23,8 +21,7 @@ def category(request, category_id):
 
 
 def recipe(request, id):
-    recipe = Recipe.objects.filter(pk=id, is_published=True).order_by('-id').first()
-
+    recipe = get_object_or_404(Recipe, pk=id, is_published=True,)
     return render(request, 'home/pages/recipe-view.html', context={
         'recipe': recipe, 'is_detail_page': True,
     })
