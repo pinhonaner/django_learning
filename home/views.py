cfbf5e499dfbaddf7a12522b665466005876fb8e
Renan Pinho <pinhorenan@icloud.com>
2023-02-27 06:36:34 -0300

    minor update

--- home/views.py
+++ home/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404, get_object_or_404 
 from utils.recipes.factory import make_recipe
+from django.http import HttpResponse, Http404
 from .models import Recipe
 
 # Create your views here.
@@ -10,15 +11,15 @@ def home(request):
     ).order_by('-id')
     return render(request, 'home/pages/home.html', context={
         'recipes': recipes,
-        'title': f'{recipes.first().category.name}'
+        
     })
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(
-        category__id=category_id,
-        is_published=True,).order_by('-id')
+    recipes = get_list_or_404(Recipe.objects.filter(category__id=category_id, is_published=True).order_by('-id'))
+
     return render(request, 'home/pages/category.html', context={
         'recipes': recipes,
+        'title': f'{recipes[0].category.name} - Category |',
     })
 
 
